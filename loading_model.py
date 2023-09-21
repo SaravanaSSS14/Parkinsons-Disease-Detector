3d09b369e71d4ac7be9a8c97559b51bf68f45011
SARAVANASUDHARSAN S <saravanasudharsan14@gmail.com>
2023-09-21 18:47:44 +0530

    September 21

---
+++ loading_model.py
@@ -0,0 +1,3 @@
+import tensorflow
+from tftrainer import model as tfmodel
+tfmodel.load_weights('../parkinson_disease_detection.h5')
--- pickle_trainer.py
+++
@@ -1,8 +0,0 @@
-
-import pandas as pd
-
-pickleFile = open("saved_trainer.pkl","rb")
-
-obj = pd.read_pickle(pickleFile)
-print (obj)
-
